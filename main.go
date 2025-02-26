14f2ad352019d59fc8ad63d12429fa128bcf8ed8
avalaune <alex.valauney01@gmail.com>
2025-02-26 15:51:36 +0100

    serveur lancer + fondation du htlm

--- main.go
+++ main.go
@@ -1,3 +1,77 @@
 package main
 
-bonjour
+import (
+	"log"
+	"net/http"
+	"text/template"
+	"time"
+)
+
+func main() {
+
+	/* 	BDD() // create database and tables
+
+	   	db, err := sql.Open("sqlite3", DBPath) // open database for nexts functions
+	   	if err != nil {
+	   		log.Fatal(err)
+	   	}
+	   	defer db.Close()
+
+	   	_, err = db.Exec(`PRAGMA foreign_keys = ON;`)
+	   	if err != nil {
+	   		log.Fatalf("pragma en sang")
+	   	}
+
+	   	sSquared := CreateStructSquare(db) // create struct for methods
+	   	sData := CreateStructData()        // create struct for all datas
+
+	   	InsertNamesInDB(db, []string{"Astuces", "Étangs", "Coins pêche", "Prises", "Bateaux", "Crustacés", "Coquillages", "Poissons"}, `INSERT INTO categories (name) VALUES (?)`)
+	   	InsertNamesInDB(db, []string{"Drowned", "Classic", "Moderator", "Administrator"}, `INSERT INTO roles (name) VALUES (?)`)
+	   	InsertNamesInDB(db, []string{"likepost", "dislikepost", "likecom", "dislikecom", "comonpost", "askmod", "reportpost", "reportcom", "adminanswer"}, `INSERT INTO types (name) VALUES (?)`)
+
+	   	FillingStruct(&sSquared, &sData) // fill the data struct with database data */
+
+	ServerCreate() // build TLS structure, and then launch server
+
+}
+
+func ServerCreate() {
+
+	// No error/logout handler directly, see handlersBasic.go/handlersLog.go for this
+	indexHandler := func(w http.ResponseWriter, r *http.Request) {
+		IndexHandler(w, r)
+	}
+
+	mux := http.NewServeMux() // Mux for multiple handlers
+	mux.HandleFunc("/", indexHandler)
+
+	server := &http.Server{
+		Addr:              ":8080",          //adresse du server
+		Handler:           mux,              // listes des handlers
+		ReadHeaderTimeout: 10 * time.Second, // temps autorisé pour lire les headers
+		WriteTimeout:      10 * time.Second, // temps maximum d'écriture de la réponse
+		IdleTimeout:       30 * time.Second, // temps maximum entre deux rêquetes
+		MaxHeaderBytes:    1 << 20,          // 1 MB // maximum de bytes que le serveur va lire
+	}
+
+	log.Println("http://localhost:8080")
+	if err := server.ListenAndServe(); err != nil { // open server with auto-signed SSL certificate
+		log.Fatal(err)
+	}
+}
+
+// function to handle the index requests
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+
+	tmpl, err := template.ParseFiles("index.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Exécute le template avec les données
+	err = tmpl.Execute(w, nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
