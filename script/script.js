3be37f7560d2845fc6dd202d0a299d0ed92d0659
alebourg <abelux662@gmail.com>
2025-02-27 21:32:37 +0100

    Commentaries v1

--- script/script.js
+++ script/script.js
@@ -1,26 +1,26 @@
 export function init() {
-    window.onload = function () {
-        let conn;
-        
+    window.onload = function () { //Launched when window is loading
+        let conn
+
+        //Creating the front websocket connection 
         if (window["WebSocket"]) {
-            conn = new WebSocket(document.location.origin);
-            conn.onclose = function (evt) {
+            conn = new WebSocket(document.location.origin)
+            conn.onclose = function (e) { //Display that the user was disconnected of the ws 
                 console.log("Closed WS")
-            };
-            conn.onmessage = function (evt) {
-                console.log(evt.data)
-            };
+            }
+            conn.onmessage = function (e) { //Will be splitted in further cases depending on the nature of the message
+                console.log(e.data)
+            }
         } else {
             console.log("Your browser does not support WebSockets")
         }
-        
-        handleForms(conn)
+        handleForms(conn) //Getting forms and using the ws
     }
     onClicksFunctions()
 }
 
 function handleForms(conn) {
-    
+    //All forms got by id
     let registerForm = document.getElementById("registerForm")
     registerForm.onsubmit = function (e) {
         onSubForm(e, registerForm)
@@ -37,7 +37,7 @@ function handleForms(conn) {
     loginForm.onsubmit = function (e) {
         onSubForm(e, loginForm)
     }
-    
+    //Local function putting in object shape forms to pass through the ws
     function onSubForm (e, form) {
         let formData = {}
         e.preventDefault()
@@ -52,19 +52,20 @@ function handleForms(conn) {
                 formData[field.name] = field.value
             }
         })
-        console.log(JSON.stringify(formData))
+        console.log(JSON.stringify(formData)) //Checking for us if everything's working fine, will be deleted later
         if (!conn) {
             return false
         }
-        conn.send(JSON.stringify(formData))
+        conn.send(JSON.stringify(formData)) //Only strings, blobs, ArrayBuffers are accepted to be sent
         return false
     }
 }
 
-
+//Events that will used with onLoadPage() to switch sections
 function onClicksFunctions() {
-    onLoadPage('register')
+    onLoadPage('register') //If unlogged, display register section, by default
     let currentLoad = document.body.querySelector('section:not(.hidden)')
+
     document.getElementById('linkLogin').onclick = function (e) {
         onLoadPage('login', currentLoad.id)
         currentLoad = document.body.querySelector('section:not(.hidden)')
@@ -76,6 +77,7 @@ function onClicksFunctions() {
     }
 }
 
+//Switching classes on sections to hide/display what we want
 function onLoadPage(newSection, oldSection) {
     document.getElementById(newSection).classList.remove('hidden')
     if (oldSection) {
