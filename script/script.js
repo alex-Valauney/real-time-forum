d3c9d792334325179841431047654046fbb7136e
alebourg <abelux662@gmail.com>
2025-02-28 13:04:44 +0100

    ws working both sides

--- script/script.js
+++ script/script.js
@@ -1,18 +1,31 @@
 export function init() {
     window.onload = function () { //Launched when window is loading
         let conn
+        console.log(document.location)
 
         //Creating the front websocket connection 
         if (window["WebSocket"]) {
-            conn = new WebSocket(document.location.origin)
+            conn = new WebSocket("ws://" + document.location.host + "/ws")
+            let output = document.querySelector("#output")
+            conn.onopen = function (e) {
+                output.textContent = "connection successful"
+            }
             conn.onclose = function (e) { //Display that the user was disconnected of the ws 
                 console.log("Closed WS")
             }
+            conn.onerror = function (e) {
+                output.textContent = "error : " + e.data
+            }
             conn.onmessage = function (e) { //Will be splitted in further cases depending on the nature of the message
-                console.log(e.data)
+                output.textContent = "received : " + e.data
             }
         } else {
             console.log("Your browser does not support WebSockets")
+        }
+        document.getElementById("testbutWS").onclick = function (e) {
+            let textinputdata = document.querySelector("#testWS").value
+            console.log(textinputdata)
+            conn.send(textinputdata)
         }
         handleForms(conn) //Getting forms and using the ws
     }
@@ -23,22 +36,22 @@ function handleForms(conn) {
     //All forms got by id
     let registerForm = document.getElementById("registerForm")
     registerForm.onsubmit = function (e) {
-        onSubForm(e, registerForm)
+        onSubForm(e, registerForm) //manque method
     }
     let loginForm = document.getElementById("loginForm")
     loginForm.onsubmit = function (e) {
-        onSubForm(e, loginForm)
+        onSubForm(e, loginForm) //manque method
     }
     let postForm = document.getElementById("postForm")
     loginForm.onsubmit = function (e) {
-        onSubForm(e, loginForm)
+        onSubForm(e, loginForm) //manque method
     }
     let commentForm = document.getElementById("commentForm")
     loginForm.onsubmit = function (e) {
-        onSubForm(e, loginForm)
+        onSubForm(e, loginForm) //manque method
     }
     //Local function putting in object shape forms to pass through the ws
-    function onSubForm (e, form) {
+    function onSubForm (e, form, method) {
         let formData = {}
         e.preventDefault()
         let fields = form.querySelectorAll("input")
@@ -52,6 +65,8 @@ function handleForms(conn) {
                 formData[field.name] = field.value
             }
         })
+        formData["method"] = method
+
         console.log(JSON.stringify(formData)) //Checking for us if everything's working fine, will be deleted later
         if (!conn) {
             return false
