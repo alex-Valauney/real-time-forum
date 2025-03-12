c558259f60c99dd97b93e49b5c0a6baa44cad5d2
damien guimiot <damienguimiot@gmail.com>
2025-03-12 16:00:45 +0100

    methode select mp avec from et to

--- databaseComs.go
+++ databaseComs.go
@@ -306,3 +306,28 @@ func (db *BDD) InsertPrivateMessage(obj map[string]any) Response {
 	}
 	return Response{newMesId}
 }
+
+func (db *BDD) SelectPMByFromTo(obj map[string]any) Response {
+	/*
+		expected input (as json object) :
+		{
+			user_from : int,
+			user_to : int,
+			method : SelectPMByFromTo
+		}
+	*/
+	stmt := "SELECT * FROM privatemessages WHERE (user_from = ? AND user_to = ?) OR (user_from = ? AND user_to = ?);"
+	result, err := db.conn.Query(stmt, obj["user_from"], obj["user_to"], obj["user_to"], obj["user_from"])
+	if err != nil {
+		fmt.Println(err)
+		return Response{[]PrivateMessage{}}
+	}
+
+	tabPrivateMessages := []PrivateMessage{}
+	for result.Next() {
+		privateMessage := PrivateMessage{}
+		result.Scan(&privateMessage.id, &privateMessage.user_from, &privateMessage.user_to, &privateMessage.content, &privateMessage.date)
+		tabPrivateMessages = append(tabPrivateMessages, privateMessage)
+	}
+	return Response{tabPrivateMessages}
+}
