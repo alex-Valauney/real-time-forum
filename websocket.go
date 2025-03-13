e05d2f5a618b84f7c8c47b04a0c7ecf54246e4ea
alebourg <abelux662@gmail.com>
2025-03-13 12:13:08 +0100

    linking session wip3

--- websocket.go
+++ websocket.go
@@ -58,8 +58,10 @@ func WebsocketHandler(w http.ResponseWriter, r *http.Request) {
 		BDDConn.CloseConn()
 
 		if obj["method"] == "Authenticate" || obj["method"] == "InsertUser" {
-			SessionGen(w, User, Bool)
-
+			if obj["remember"] == nil {
+				obj["remember"] = false
+			}
+			SessionGen(w, result.Result.(User), obj["remember"].(bool))
 		}
 
 		err = conn.WriteJSON(result)
