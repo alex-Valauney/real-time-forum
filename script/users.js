0708dc274e87c7c1fbc1a14de0d2daee9c46ddc5
alebourg <abelux662@gmail.com>
2025-03-19 17:44:21 +0100

    Userlist from JS v1

---
+++ script/users.js
@@ -0,0 +1,76 @@
+ function sortUser(allUsers, onlineUsers, pmClient, currentClient) {
+    let offlineUsers = allUsers.filter(user => !onlineUsers.includes(user))
+    onlineUsers = onlineUsers.filter(user => user.Id !== currentClient)
+    onlineUsers.sort((a, b) => sortByPm(a, b, pmClient))
+    offlineUsers.sort((a, b) => sortByPm(a, b, pmClient))
+
+    addUserElem(onlineUsers, true, pmClient)
+    addUserElem(offlineUsers, false, pmClient)
+}
+
+function sortByPm(userA, userB, pmClient) {
+    const inPmA = pmClient.includes(userA.Id)
+    const inPmB = pmClient.includes(userB.Id)
+
+    if (inPmA && !inPmB) {
+        return -1
+    }
+    if (!inPmA && inPmB) {
+        return 1
+    }
+    if (inPmA && inPmB) {
+        return pmClient.indexOf(userA.Id) - pmClient.indexOf(userB.Id)
+    }
+
+    return userA.User_nickname.localeCompare(userB.User_nickname)
+}
+
+function addUserElem(tabUser, online, pmClient) {
+    const userListOn = document.getElementById("onlineUser")
+    const userListOff = document.getElementById("offlineUser")
+    tabUser.forEach(user => {
+        let userDiv = createUserElem(user, online, pmClient)
+        if (online) {
+            userListOn.appendChild(userDiv)
+        } else {
+            userListOff.appendChild(userDiv)
+        }
+    })
+    attachPostClickEvents()
+}
+
+function createUserElem(user, online, pmClient) {
+    let pmIndexUser = pmClient.filter(pm => user.Id === pm.User_from || user.Id === pm.User_to)
+    let lastDate
+    if (pmIndexUser) {
+        lastDate = pmIndexUser[0].Date
+    }
+
+    const userDiv = document.createElement("div")
+
+    const usernameDiv = document.createElement("div")
+    const usernameText = document.createElement("span")
+    usernameText.textContent = user.User_nickname
+    usernameDiv.appendChild(usernameLabel)
+    usernameDiv.appendChild(usernameText)
+
+    const lastMessageDiv = document.createElement("div")
+    const lastMessageLabel = document.createElement("span")
+    lastMessageLabel.textContent = "Last contact: "
+    const lastMessageText = document.createElement("span")
+    lastMessageText.textContent = lastDate ? lastDate : ""
+    lastMessageDiv.appendChild(lastMessageLabel)
+    lastMessageDiv.appendChild(lastMessageText)
+   
+    userDiv.appendChild(usernameDiv)
+    userDiv.appendChild(lastMessageDiv)
+
+    if (online) {
+        const chatButton = document.createElement("button")
+        chatButton.textContent = "Ouvrir Chat"
+        chatButton.onclick = () => openChat(userName)
+        userDiv.appendChild(chatButton)
+    }   
+
+    return userDiv
+}
