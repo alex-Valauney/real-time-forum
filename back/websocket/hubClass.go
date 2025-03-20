57cf72add0752e8421aa03f043d631a5abfc6bc2
avalaune <alex.valauney01@gmail.com>
2025-03-20 12:23:18 +0100

    select et case

--- back/websocket/hubClass.go
+++ back/websocket/hubClass.go
@@ -2,16 +2,29 @@ package websocket
 
 type Hub struct {
 	Clients map[*Client]bool
+
+	Connection chan *Client
+
+	Deconnection chan *Client
+
+	Buffer chan []byte
 }
 
 func NewHub() *Hub {
 	return &Hub{
-		Clients: map[*Client]bool{},
+		Clients:      map[*Client]bool{},
+		Connection:   make(chan *Client),
+		Deconnection: make(chan *Client),
+		Buffer:       make(chan []byte),
 	}
 }
 
 func (h *Hub) run() {
 	for {
-		select {}
+		select {
+		case client := <-h.Connection:
+		case client := <-h.Deconnection:
+		case message := <-h.Buffer:
+		}
 	}
 }
