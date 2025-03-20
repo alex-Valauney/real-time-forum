dffb0d668f6e9b7333d67f66a420181807189986
avalaune <alex.valauney01@gmail.com>
2025-03-20 11:53:50 +0100

    hub class newHub

--- back/websocket/hubClass.go
+++ back/websocket/hubClass.go
@@ -1,1 +1,19 @@
 package websocket
+
+import "container/list"
+
+type Hub struct {
+	Clients []*Client
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		Clients: []*Client{},
+	}
+}
+
+func (h *Hub) run() {
+	for {
+		select
+	}
+}
