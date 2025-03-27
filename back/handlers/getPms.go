13d3fcf2f870b8dfe2f8dc6ac1e75d3c25f179bc
damien guimiot <damienguimiot@gmail.com>
2025-03-27 10:07:11 +0100

    endpoint pm simple

--- back/handlers/getPms.go
+++ back/handlers/getPms.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 	"rtf/back/methods"
+	"slices"
+	"strconv"
 )
 
 func SpepmHandler(w http.ResponseWriter, r *http.Request) {
@@ -22,4 +24,55 @@ func SpepmHandler(w http.ResponseWriter, r *http.Request) {
 	BDDConn.CloseConn()
 
 	encoder.Encode(result.Result)
+}
+
+func PmHandler(w http.ResponseWriter, r *http.Request) {
+	idClient := r.URL.Query().Get("id")
+	encoder := json.NewEncoder(w)
+
+	if idClient == "" {
+		encoder.Encode([]methods.PrivateMessage{})
+		return
+	}
+
+	stmt := "SELECT * FROM privatemessages WHERE user_to = ? OR user_from = ? ORDER BY id DESC;"
+
+	BDDConn := &methods.BDD{}
+	BDDConn.OpenConn()
+	rows, err := BDDConn.Conn.Query(stmt, idClient, idClient)
+	BDDConn.CloseConn()
+
+	if err != nil {
+		encoder.Encode([]methods.PrivateMessage{})
+		return
+	}
+
+	tabAllPm := []methods.PrivateMessage{}
+	for rows.Next() {
+		pm := methods.PrivateMessage{}
+
+		rows.Scan(&pm.Id, &pm.User_from, &pm.User_to, &pm.Content, &pm.Date)
+
+		tabAllPm = append(tabAllPm, pm)
+	}
+
+	tabFilteredPm := []methods.PrivateMessage{}
+	tabDupeID := []int{}
+	idClientInt, _ := strconv.Atoi(idClient)
+
+	for _, pm := range tabAllPm {
+		idOther := 0
+		if pm.User_from != idClientInt {
+			idOther = pm.User_from
+		} else if pm.User_to != idClientInt {
+			idOther = pm.User_to
+		}
+
+		if !slices.Contains(tabDupeID, idOther) {
+			tabDupeID = append(tabDupeID, idOther)
+			tabFilteredPm = append(tabFilteredPm, pm)
+		}
+	}
+
+	encoder.Encode(tabFilteredPm)
 }
